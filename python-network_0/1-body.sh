26ff5e8befba56b44ce4461f973357871dd750c6
AkbarX05 <13619@holbertonstudents.com>
2026-04-17 22:41:06 +0400

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Sends a GET request and displays body only if status code is 200
 response=$(curl -s -w "%{http_code}" "$1")
 body=$(echo "$response" | sed '$d')
 status=$(echo "$response" | tail -n1)
