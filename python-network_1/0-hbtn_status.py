8100d86173d01774bf362a2087572962d09c452b
AkbarX05 <13619@holbertonstudents.com>
2026-04-24 18:31:21 +0000

    pyt

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
+ #!/usr/bin/python3
 import urllib.request
 
 url = 'https://intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
-    
     print("Body response:")
     print(f"\t- type: {type(body)}")
     print(f"\t- content: {body}")
