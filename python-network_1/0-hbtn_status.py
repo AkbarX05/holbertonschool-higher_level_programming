48c8a00653755611bc619a1f73108b39657180dd
AkbarX05 <13619@holbertonstudents.com>
2026-04-24 22:28:43 +0400

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,6 @@ url = 'https://intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
-    
     print("Body response:")
     print(f"\t- type: {type(body)}")
     print(f"\t- content: {body}")
